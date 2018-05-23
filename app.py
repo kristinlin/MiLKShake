74862058c81e098bf9359d7e20e6b2a85a0a9d86
sonalparab <sparab@stuy.edu>
2018-05-22 20:41:06 -0400

    worked on login & signup

--- app.py
+++ app.py
@@ -15,13 +15,36 @@ def root():
 def login():
     return render_template('login.html',title='Login')
 
+@app.route('/signup/', methods = ['POST','GET'])
+def signup():
+    username = request.form['user']
+    password = request.form['pass']
+    if username == '':
+        flash('Please enter a username')
+        return redirect(url_for('login'))
+    if password == '':
+        flash('Please enter a password')
+        return redirect(url_for('login'))
+    #check if user in database
+    #if not, add the user & pass
+    flash('You clicked the Sign Up button!')
+    flash('Functionality to be implemented later')
+    #flash(user + 'succesfully created')
+    return redirect(url_for('login'))
+
 @app.route('/welcome/', methods = ['POST','GET'])
 def welcome():
     if 'username' in session:
         return render_template('welcome.html',username=session['username'])
     else:
-        username = request.form['name']
+        username = request.form['user']
         password = request.form['pass']
+    if username == '':
+        flash('Please enter a username')
+        return redirect(url_for('login'))
+    if password == '':
+        flash('Please enter a password')
+        return redirect(url_for('login'))
     if username in dict:
         if dict[username] == password:
             session['username'] = username
@@ -29,10 +52,10 @@ def welcome():
             return redirect(url_for('welcome'))
     if username in dict:
         flash('Bad password')
-        return redirect(url_for('root'))
+        return redirect(url_for('login'))
     else:
         flash('Bad username')
-        return redirect(url_for('root'))
+        return redirect(url_for('login'))
 
 @app.route('/logout/',methods = ['POST','GET'])
 def logout():
