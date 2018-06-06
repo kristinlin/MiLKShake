6618844fd79d1279667e067db7ecf86dca1fd30d
kristinlin <klin11@stuy.edu>
2018-06-06 14:06:16 -0400

    did color select

--- dairynotes/static/notetype.js
+++ dairynotes/static/notetype.js
@@ -47,7 +47,7 @@ var add = function(e) {
 add_item.addEventListener("click", add);
 
 
-
+var color_opt = ["#274060", "#CC4933", "#F86624", "#E89005", "#5A9367", "#824C71"];
 var color_select = document.getElementById("color");
 var blueopt = document.getElementById("blueopt");
 var redopt = document.getElementById("redopt");
@@ -57,16 +57,36 @@ var purpopt = document.getElementById("purpopt");
 var yelopt = document.getElementById("yelopt");
 var colors = [blueopt, redopt, orgopt, yelopt, greopt, purpopt];
 
+
 var ch_color = function(val) {
+    console.log(color_select);
     console.log(color_select.style.backgroundColor);
-    color_select.style.backgroundColor = colors[val].style.backgroundColor;
+    color_select.style.backgroundColor = color_opt[val];
     console.log(color_select.style.backgroundColor);
 };
 
-for (i = 0; i< colors.length; i++) {
-    colors[i].addEventListener("mouseover",
-			       function(e) {
-				   ch_color(i);
-			       }
-			      );
-}
+colors[0].addEventListener("click",
+			   function(e) {
+			       console.log(0);
+			       ch_color(0);
+			   });
+colors[1].addEventListener("click",
+			   function(e) {
+			       ch_color(1);
+			   });
+colors[2].addEventListener("click",
+			   function(e) {
+			       ch_color(2);
+			   });
+colors[3].addEventListener("click",
+			   function(e) {
+			       ch_color(3);
+			   });
+colors[4].addEventListener("click",
+			   function(e) {
+			       ch_color(4);
+			   });
+colors[5].addEventListener("click",
+			   function(e) {
+			       ch_color(5);
+			   });
