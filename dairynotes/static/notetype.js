5adc05047138de70ea6a73cc7030312e52660f17
Kristin Lin <klin11@stuy.edu>
2018-06-13 05:55:49 -0400

    removed image things

--- dairynotes/static/notetype.js
+++ dairynotes/static/notetype.js
@@ -8,36 +8,26 @@ var CHECKBOX = 1;
 var IMAGE = 2;
 
 var tog1 = document.getElementById("tog1");
-var tog2 = document.getElementById("tog2");
 
 var reg = document.getElementById("typenote");
 var checkbox = document.getElementById("checkbox_note");
-var image = document.getElementById("image_note");
 
-var opt_name = ["Checklist", "Image", "Note"];
-var opt_val = [checkbox, image, reg];
-var but_opt = [tog1, tog2];
+var note = true;
 
-var swap = function(arr, val1, val2) {
-    var temp = arr[val1];
-    arr[val1] = arr[val2];
-    arr[val2] = temp;
-};
-
-var toggle = function(val) {
-    console.log(val);
+var toggle = function() {
     //switch the hidden vals
-    opt_val[val].hidden = false;
-    opt_val[2].hidden = true;
+    reg.hidden= !reg.hidden;
+    checkbox.hidden = !checkbox.hidden;
     //set the button triggered to that option
-    but_opt[val].textContent = opt_name[2];
-    //swap
-    swap(opt_name, val, 2);
-    swap(opt_val, val, 2);
+    if (note) {
+	tog1.textContent = "checkbox";
+    } else {
+	tog1.textContent ="note";
+    }
+    note = !note;
 };
 
-tog1.addEventListener("click", function(e) {toggle(0);});
-tog2.addEventListener("click", function(e) {toggle(1);});
+tog1.addEventListener("click", toggle);
 
 
 
